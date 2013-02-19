691236ec1a13b8ff593547f39112eba6fd95b193
AJ Bourg <aj.bourg@sendgrid.com>
2013-02-19 15:53:47 -0700

    My puppetry is rusty

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -7,15 +7,20 @@ node default {
 		user	=> root,
 	}
 	
+	# crap I always want installed but never seems to be...
+	package { ['vim', 'git-core']:
+	    ensure => "present"
+	}
+	
 	# dependencies for building gems
-	Package { ['ruby1.9.1-dev', 'rubygems', 'ruby1.9.1-full']:
+	package { ['ruby1.9.1-dev', 'rubygems', 'ruby1.9.1-full']:
 	    ensure => "present"
 	}
 	
 	# bootstrap being able to run puppet librarian for our Puppet file
-	Package { 'librarian-puppet':
+	package { 'librarian-puppet':
         ensure      => 'installed',
         provider    => 'gem',
-        require     => [Package['ruby1.9.1-dev'], Package['rubygems'], Package['ruby1.9.1-full']]
+        require     => [ Package['ruby1.9.1-dev'], Package['rubygems'], Package['ruby1.9.1-full'] ]
   }
 }
